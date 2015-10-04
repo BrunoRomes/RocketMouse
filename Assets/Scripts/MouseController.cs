27483324db6b00fc81777d41ca9b575199773c1b
Bruno Lima <brunoromes@gmail.com>
2015-10-04 16:27:30 -0300

    Tutorial Part 3: lasers, coins, death and parallax

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -13,6 +13,83 @@ public class MouseController : MonoBehaviour {
 
 	public ParticleSystem jetpack;
 
+	private bool dead = false;
+
+	private uint coins = 0;
+
+	public Texture2D coinIconTexture;
+
+	public AudioClip coinCollectSound;
+
+	public AudioSource jetpackAudio;
+	
+	public AudioSource footstepsAudio;
+
+	public ParallaxScroll parallax;
+
+	void AdjustFootstepsAndJetpackSound(bool jetpackActive)    
+	{
+		footstepsAudio.enabled = !dead && grounded;
+		
+		jetpackAudio.enabled =  !dead && !grounded;
+		jetpackAudio.volume = jetpackActive ? 1.0f : 0.5f;        
+	}
+
+	void DisplayCoinsCount()
+	{
+		Rect coinIconRect = new Rect(10, 10, 32, 32);
+		GUI.DrawTexture(coinIconRect, coinIconTexture);                         
+		
+		GUIStyle style = new GUIStyle();
+		style.fontSize = 30;
+		style.fontStyle = FontStyle.Bold;
+		style.normal.textColor = Color.yellow;
+		
+		Rect labelRect = new Rect(coinIconRect.xMax, coinIconRect.y, 60, 32);
+		GUI.Label(labelRect, coins.ToString(), style);
+	}
+
+	void DisplayRestartButton()
+	{
+		if (dead && grounded)
+		{
+			Rect buttonRect = new Rect(Screen.width * 0.35f, Screen.height * 0.45f, Screen.width * 0.30f, Screen.height * 0.1f);
+			if (GUI.Button(buttonRect, "Tap to restart!"))
+			{
+				Application.LoadLevel (Application.loadedLevelName);
+			};
+		}
+	}
+
+	void OnGUI()
+	{
+		DisplayCoinsCount();
+		DisplayRestartButton();
+	}
+
+	void CollectCoin(Collider2D coinCollider)
+	{
+		coins++;
+		Destroy(coinCollider.gameObject);
+		AudioSource.PlayClipAtPoint(coinCollectSound, transform.position);
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.gameObject.CompareTag("coins"))
+			CollectCoin(collider);
+		else
+			HitByLaser(collider);
+	}
+	
+	void HitByLaser(Collider2D laserCollider)
+	{
+		if (!dead)
+			laserCollider.gameObject.GetComponent<AudioSource>().Play();
+		dead = true;
+		animator.SetBool("dead", true);
+	}
+
 	public void Start(){
 		animator = GetComponent<Animator> ();
 	}
@@ -27,17 +104,27 @@ public class MouseController : MonoBehaviour {
 	{
 		bool jetpackActive = Input.GetButton("Fire1");
 		
+		jetpackActive = jetpackActive && !dead;
+		
 		if (jetpackActive)
 		{
 			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jetpackForce));
 		}
-
-		Vector2 newVelocity = GetComponent<Rigidbody2D>().velocity;
-		newVelocity.x = forwardMovementSpeed;
-		GetComponent<Rigidbody2D>().velocity = newVelocity;
-
+		
+		if (!dead)
+		{
+			Vector2 newVelocity = GetComponent<Rigidbody2D>().velocity;
+			newVelocity.x = forwardMovementSpeed;
+			GetComponent<Rigidbody2D>().velocity = newVelocity;
+		}
+		
 		UpdateGroundedStatus();
+		
 		AdjustJetpack(jetpackActive);
+
+		AdjustFootstepsAndJetpackSound(jetpackActive);
+
+		parallax.offset = transform.position.x;
 	}
 
 	void UpdateGroundedStatus()
