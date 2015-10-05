c870757b30ac03e2016bb9a5661477c546a1e3b6
Bruno Lima <brunoromes@gmail.com>
2015-10-04 22:17:13 -0300

    GUI Tutorial Part 2: animations and bugs in clicks

--- Assets/Scripts/UIManagerScript.cs
+++ Assets/Scripts/UIManagerScript.cs
@@ -3,14 +3,23 @@ using System.Collections;
 
 public class UIManagerScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public Animator startButton;
+	public Animator settingsButton;
+	public Animator dialog;
+
+	public void OpenSettings()
+	{
+		startButton.SetBool("isHidden", true);
+		settingsButton.SetBool("isHidden", true);
+		dialog.enabled = true;
+		dialog.SetBool("isHidden", false);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void CloseSettings()
+	{
+		startButton.SetBool("isHidden", false);
+		settingsButton.SetBool("isHidden", false);
+		dialog.SetBool("isHidden", true);
 	}
 
 	public void StartGame()
